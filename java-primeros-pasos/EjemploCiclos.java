ff6f9f6268cf99fd788707d173e10fd331e080a2
Mariquena Allosa <allosamariquena@gmail.com>
2023-02-17 18:05:36 -0300

    While

---
+++ java-primeros-pasos/EjemploCiclos.java
@@ -0,0 +1,16 @@
+
+public class EjemploCiclos {
+	public static void main(String[] args) {
+		
+	int contador = 0 ;
+	
+	while(contador <= 10) {
+		// Ejecuta esto
+		System.out.println(contador);
+		//contador +=1;
+		contador++;
+	}
+		
+	}
+
+}

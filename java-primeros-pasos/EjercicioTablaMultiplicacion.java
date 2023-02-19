083607bb9107ac85ddfa5de4e41de43a3b65b6f8
Mariquena Allosa <allosamariquena@gmail.com>
2023-02-19 12:34:50 -0300

    Ciclos anidados
    Generar tablas de multiplicacion

---
+++ java-primeros-pasos/EjercicioTablaMultiplicacion.java
@@ -0,0 +1,17 @@
+public class EjercicioTablaMultiplicacion {
+	
+	public static void main(String[] args) {
+		
+		for (int contador = 1 ; contador <=10; contador++) {		
+			for(int multiplicacion = 0 ; multiplicacion <=10; multiplicacion++) {
+				System.out.print(contador*multiplicacion);
+				System.out.print(" ");
+								
+			}
+			System.out.println();
+		}
+			
+		}
+		
+	}
+

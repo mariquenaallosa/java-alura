53055f6fe812e41f2d0d1c8b9db587ac00288c96
Mariquena <allosamariquena@gmail.com>
2023-04-16 17:29:22 -0300

    Create AutenticacionUtil.java

---
+++ java-oop/bytebank-heredado/AutenticacionUtil.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class AutenticacionUtil {
+
+	private String clave;
+	
+	public boolean iniciarSesion(String clave) {
+		if (this.clave == clave) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+}

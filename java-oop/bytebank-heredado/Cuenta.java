651a5317c0ea0053c4d6e3aef7fb0faced6a816c
Mariquena <allosamariquena@gmail.com>
2023-04-16 17:28:29 -0300

    Cliente y cuenta

---
+++ java-oop/bytebank-heredado/Cuenta.java
@@ -0,0 +1,105 @@
+package bytebank;
+// entidad cuenta
+public abstract class Cuenta {
+
+	protected double saldo;
+	private int agencia = 1;
+	private int numero;
+	private Cliente titular = new Cliente();
+
+	private static int total ;
+	
+	public Cuenta () {};
+
+	// Contructor
+	public Cuenta(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("No se permite 0");
+			this.agencia = 1;
+		} else {
+			this.agencia = agencia;
+		}
+		total++;
+		System.out.println("Se van creando: " + total + " cuentas.");
+
+	}
+	
+	public Cuenta(int agencia, int numero) {
+		if (agencia <= 0) {
+			System.out.println("No se permite 0");
+			this.agencia = 1;
+		} else {
+			this.agencia = agencia;
+		}
+		total++;
+		System.out.println("Se van creando: " + total + " cuentas.");
+
+	}
+
+	// NO retorna valor
+	public abstract void depositar(double saldo);
+	
+	
+	// Retorna valor
+	public boolean retirar(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	boolean transferir(double valor, Cuenta cuenta) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			cuenta.saldo += valor;
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public static int getTotal() {
+		return Cuenta.total;
+	}
+	
+	public void setAgencia(int nuevaAgencia) {
+		if (nuevaAgencia > 0) {
+			this.agencia = nuevaAgencia;
+		} else {
+			System.out.println("No están permitidos números negativos");
+		}
+	}
+
+	public void setNumero(int nuevoNumero) {
+		if (nuevoNumero > 0) {
+			this.numero = nuevoNumero;
+		} else {
+			System.out.println("No están permitidos números negativos");
+
+		}
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+}

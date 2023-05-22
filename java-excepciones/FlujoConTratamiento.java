d59615cd4af1ef3ed7b61d50c8b4529c2dff2446
Mariquena <allosamariquena@gmail.com>
2023-05-22 10:41:41 -0300

    Tratamiento de varias excepciones

--- java-excepciones/FlujoConTratamiento.java
+++ java-excepciones/FlujoConTratamiento.java
@@ -2,18 +2,25 @@
 public class FlujoConTratamiento {
 	public static void main(String[] args) {
 		System.out.println("Inicio main");
-		metodo1();
+		try{
+			metodo1();
+		}catch(MiException | ArithmeticException | NullPointerException  e) {
+			e.printStackTrace();
+		}
 		System.out.println("Fin main");
 	}
 
-	public static void metodo1() {
+	public static void metodo1() throws MiException {
 		System.out.println("Inicio de metodo1");
 		metodo2();
 		System.out.println("Fin de metodo1");
 	}
 
-	public static void metodo2() {
-		System.out.println("Inicio de metodo2");
+	public static void metodo2() throws MiException {
+		System.out.println("Inicio de metodo 2");
+		int a = 50/0; // Aritmethic exception
+		Cuenta c = null;
+		c.deposita();
 		 throw new ArithmeticException("Surgió un error");
 	}
 }

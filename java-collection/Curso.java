76c73547c5fe89207b53ddc595d09ebade296384
Mariquena <allosamariquena@gmail.com>
2023-05-29 20:55:16 -0300

    Sets e Iterators

--- java-collection/Curso.java
+++ java-collection/Curso.java
@@ -7,6 +7,7 @@ public class Curso {
     private int tiempo;
     private List<Aula> aulaList = new ArrayList<>();
     private Collection<Alumno> alumnos = new HashSet<>();
+    //private Collection<Alumno> alumnos = new LinkedHashSet<>();
 
     public Curso(String nombre, int tiempo) {
         this.nombre = nombre;
